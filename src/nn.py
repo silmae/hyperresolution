3c121b4215ce4430e05875c3fc172b37a71f5fca
Leevi <julejoli@jyu.fi>
2023-03-07 17:35:01 +0200

    Separated main into its own file, added init to src

--- src/nn.py
+++ src/nn.py
@@ -1,10 +1,4 @@
-"""
-Leaf model implementation as a neural network.
 
-Greatly accelerates prediction time compared to the original
-optimization method with some loss to accuracy.
-
-"""
 
 import logging
 import sys
@@ -25,7 +19,7 @@ import torchmetrics
 import scipy.io # for loading Matlab matrices
 import matplotlib.pyplot as plt
 
-import plotter
+from src import plotter
 
 # Set manual seed when doing hyperparameter search for comparable results
 # between training runs.
@@ -33,7 +27,7 @@ torch.manual_seed(42)
 
 bands = 80
 
-torch.autograd.set_detect_anomaly(True) # this will provide traceback if stuff turns into NaN
+torch.autograd.set_detect_anomaly(True)  # this will provide traceback if stuff turns into NaN
 
 def SAM(s1, s2):
     """
@@ -405,96 +399,3 @@ def train(epochs=1):
     return best_loss
 
 
-if __name__ == '__main__':
-    # log to stdout instead of stderr for nice coloring
-    logging.basicConfig(stream=sys.stdout, level='INFO')
-
-    train(epochs=15000)
-
-
-
-# The rest is abandoned code saved for snippets if needed
-
-# def predict(r_m, t_m, nn_name='nn_default'):
-#     """Use neural network to predict HyperBlend leaf model parameters from measured reflectance and transmittance.
-#
-#     :param r_m:
-#         Measured reflectance.
-#     :param t_m:
-#         Measured transmittance.
-#     :param nn_name:
-#         Neural network name. Default name 'nn_default' is used if not given.
-#         Provide only if you want to use your trained custom NN.
-#     :return:
-#         Lists ad, sd, ai, mf (absorption density, scattering desnity, scattering anisotropy, and mixing factor).
-#         Use ``leaf_commons._convert_raw_params_to_renderable()`` before passing them to rendering method.
-#     """
-#
-#     net = _load_model(nn_name=nn_name)
-#     r_m = np.array(r_m)
-#     t_m = np.array(t_m)
-#     res = net(from_numpy(np.column_stack([r_m, t_m])))
-#     res_item = res.detach().numpy()
-#     ad = np.clip(res_item[:,0], 0., 1.)
-#     sd = np.clip(res_item[:,1], 0., 1.)
-#     ai = np.clip(res_item[:,2], 0., 1.)
-#     mf = np.clip(res_item[:,3], 0., 1.)
-#     return ad, sd, ai, mf
-
-
-# def _load_model(nn_name):
-#     """Loads the NN from disk.
-#
-#     :param nn_name:
-#         Name of the model file.
-#     :return:
-#         Returns loaded NN.
-#     :exception:
-#         ModuleNotFoundError can happen if the network was trained when the name
-#         of this script was something else than what it is now. Your only help
-#         is to train again.
-#     """
-#     try:
-#         p = _get_model_path(nn_name)
-#         net = load(p)
-#         net.eval()
-#         logging.info(f"NN model loaded from '{p}'")
-#     except ModuleNotFoundError as e:
-#         logging.error(f"Pytorch could not load requested neural network. "
-#                       f"This happens if class or file names associated with "
-#                       f"NN are changed. You must train a new model to fix this.")
-#         raise
-#     return net
-
-
-# def _get_model_path(nn_name='nn_default'):
-#     """Returns path to the NN model.
-#
-#     :param nn_name:
-#         Name of the NN.
-#     :return:
-#         Returns path to the NN model.
-#     :exception:
-#         FileNotFoundError if the model cannot be found.
-#     """
-#
-#     if not nn_name.endswith('.pt'):
-#         nn_name = nn_name + '.pt'
-#     model_path = PH.join(PH.path_directory_surface_model(), nn_name)
-#     if os.path.exists(model_path):
-#         return model_path
-#     else:
-#         raise FileNotFoundError(f"Model '{model_path}' was not found. Check spelling.")
-
-
-# def exists(nn_name='nn_default.pt'):
-#     """Checks whether NN with given name exists.
-#
-#     :return:
-#         True if found, False otherwise.
-#     """
-#
-#     if not nn_name.endswith('.pt'):
-#         nn_name = nn_name + '.pt'
-#     model_path = PH.join(PH.path_directory_surface_model(), nn_name)
-#     return os.path.exists(model_path)
