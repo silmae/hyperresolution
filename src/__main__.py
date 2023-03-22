f34a4de11d8671c90dcb9aa8934da93d1743d8e6
Leevi <julejoli@jyu.fi>
2023-03-22 16:04:25 +0200

    Added prints about GPU usage

--- src/__main__.py
+++ src/__main__.py
@@ -3,6 +3,8 @@ import logging
 import sys
 import os
 
+import torch
+
 from src import nn
 
 
@@ -14,10 +16,17 @@ if __name__ == '__main__':
     # For running with GPU on server (having these lines here shouldn't hurt when running locally without GPU)
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     # Check available GPU with command nvidia-smi in terminal, pick one that is not in use
-    os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     ############################
 
-    print('testing')
+    print(f"Is CUDA supported by this system? {torch.cuda.is_available()}")
+    print(f"CUDA version: {torch.version.cuda}")
+
+    # Storing ID of current CUDA device
+    cuda_id = torch.cuda.current_device()
+    print(f"ID of current CUDA device: {torch.cuda.current_device()}")
+
+    print(f"Name of current CUDA device: {torch.cuda.get_device_name(cuda_id)}")
 
     # TODO Docstrings everywhere
 
