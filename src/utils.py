8a94d7aa3ba566065675f4fa6768de17af6c50d5
Leevi <julejoli@jyu.fi>
2023-05-23 20:14:35 +0300

    New file for utility functions, added apply_circular_mask

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+""""
+This file contains miscellaneous utility functions
+"""
+import numpy as np
+
+
+def apply_circular_mask(data, h, w, center=None, radius=None):
+    """From https://stackoverflow.com/a/44874588"""
+
+    if center is None:  # use the middle of the image
+        center = (int(w / 2), int(h / 2))
+    if radius is None:  # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w - center[0], h - center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2)
+
+    mask = dist_from_center <= radius
+    mask = abs((mask * 1))  # the above returns a mask of booleans, this converts it to int (somehow)
+
+    masked_data = data * mask
+    return masked_data
