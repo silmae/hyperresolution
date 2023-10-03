3af759f6a93c6f9b5cb434fb70f65415631be33e
Leevi <julejoli@jyu.fi>
2023-10-03 17:10:42 +0300

    New function for making ISIS data look like ASPECT data, reworked function for cropping and masking to include only cropping and work only with numpy data instead of torch tensors

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,7 @@ from src import constants
 
 
 def apply_circular_mask(data: np.ndarray or torch.Tensor, h: int, w: int, center: tuple = None,
-                        radius: int = None) -> np.ndarray or torch.Tensor:
+                        radius: int = None, masking_value=1) -> np.ndarray or torch.Tensor:
     """
     Create a circular mask and apply it to an image cube. Works for ndarrays and torch tensors.
     Mask creation from https://stackoverflow.com/a/44874588
@@ -29,6 +29,8 @@ def apply_circular_mask(data: np.ndarray or torch.Tensor, h: int, w: int, center
         Center of the mask; if not given, will use center of the data
     :param radius:
         Radius of the mask, in pixels; if not given, will use the largest possible radius which will fit the whole circle
+    :param masking_value:
+        Value to be used to replace the masked values. Default is 1, other useful values are np.nan (or float('nan')) and 0.
     :return:
         Masked datacube
     """
@@ -42,102 +44,85 @@ def apply_circular_mask(data: np.ndarray or torch.Tensor, h: int, w: int, center
     dist_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2)
 
     mask = dist_from_center <= radius
+
+    # # Plot to check mask shape
+    # plt.imshow(mask)
+    # plt.show()
     if type(data) == Tensor:
         device = data.device  # Check where the data is: GPU or CPU
         mask = Tensor(mask).to(device)  # Convert mask to tensor and move it to same device as data
         masked_data = data * mask
-        masked_data = masked_data + Tensor(
-            abs(mask - 1))  # Convert masked values to ones instead of zeros to avoid problems with backprop
+        masked_data = torch.where(masked_data == 0, masking_value, masked_data)
+        # masked_data = masked_data + Tensor(
+        #     abs(mask - 1))  # Convert masked values to ones instead of zeros to avoid problems with backprop
     else:
-        mask = abs((mask * 1))  # the above returns a mask of booleans, this converts it to int (somehow)
-        masked_data = data * mask
+        # mask = abs((mask * 1))  # the above returns a mask of booleans, this converts it to int (somehow)
+        masked_data = data * np.expand_dims(mask, axis=2)  # Need to have the same number of dimensions on mask and data
+        masked_data = np.where(masked_data == 0, masking_value, masked_data)
 
     return masked_data
 
 
-def crop_and_mask(dataset, aspect_ratio=1, radius=None):
+def crop2aspect_ratio(cube, aspect_ratio=1, keep_dim=None):
     """
-    Crop X, Y, and cube of a torch dataset according to aspect ratio given as parameter. Then create circular mask
-    according to radius parameter, and apply it to the cropped data.
-    :param dataset:
-        torch dataset object with X, Y, and cube
+    Crop a spectral image cube (numpy ndarray) according to aspect ratio given as parameter.
+    :param cube:
+        Spectral image cube to be cropped, dimension order (h, w, l)
     :param aspect_ratio:
         Desired aspect ratio of cropped data, default is 1/1
-    :param radius:
-        Radius of applied circular mask, in pixels
-    :return:
-        torch dataset object with X, Y, and cube cropped and masked
+    :param keep_dim: int, 0 or 1
+        Index of dimension IN THE INPUT CUBE that should be kept the same, default is None
+    :return: cube:
+        Cropped image cube as ndarray
     """
 
-    orig_w = dataset.w
-    orig_h = dataset.h
+    # Data dimension order is (h, w, l)
+    orig_h = cube.shape[0]
+    orig_w = cube.shape[1]
 
-    # Data dimension order is (l, w, h)
-
-    def cut_horizontally(dataset, h):
-        '''Make two horizontal cuts removing data from top and bottom rows of image'''
+    def cut_horizontally(cube, h):
+        """Make two horizontal cuts removing data from top and bottom rows of image"""
         half_leftover = (orig_h - h) / 2
         start_i = math.floor(half_leftover)
         end_i = math.ceil(half_leftover)
+        cube = cube[start_i:-end_i, :, :]
 
-        dataset.X = dataset.X[:, :, start_i:-end_i]
-        dataset.Y = dataset.Y[:, :, start_i:-end_i]
-        dataset.cube = dataset.cube[:, :, start_i:-end_i]
+        # cube.h = h
+        return cube
 
-        dataset.h = h
-        return dataset
-
-    def cut_vertically(dataset, w):
-        '''Make two vertical cuts removing data from left and right of center'''
+    def cut_vertically(cube, w):
+        """Make two vertical cuts removing data from left and right of center"""
         half_leftover = (orig_w - w) / 2
         start_i = math.floor(half_leftover)
         end_i = math.ceil(half_leftover)
 
-        dataset.X = dataset.X[:, start_i:-end_i, :]
-        dataset.Y = dataset.Y[:, start_i:-end_i, :]
-        dataset.cube = dataset.cube[:, start_i:-end_i, :]
+        cube = cube[:, start_i:-end_i, :]
 
-        dataset.w = w
-        return dataset
+        # cube.w = w
+        return cube
 
     if orig_h > orig_w:  # if image is not horizontal or square, rotate 90 degrees
-        dataset.X = torch.rot90(dataset.X, dims=(1, 2))
-        dataset.Y = torch.rot90(dataset.Y, dims=(1, 2))
-        dataset.cube = np.rot90(dataset.cube, axes=(1, 2))
+        cube = np.rot90(cube, axes=(0, 1))
 
-        dataset.w = orig_h
-        dataset.h = orig_w
+        w = orig_h
+        h = orig_w
+        # I am so sorry, this is confusing
+        orig_h = h
+        orig_w = w
 
-        orig_h = dataset.h
-        orig_w = dataset.w
+        if keep_dim is not None:
+            keep_dim = abs(keep_dim - 1)  # if rotated, dimension to be kept the same changes from 0 to 1, or 1 to 0
 
-    if orig_w > int(orig_h * aspect_ratio):
+    if orig_w > int(orig_h * aspect_ratio) and keep_dim != 1:
         h = orig_h
         w = int(orig_h * aspect_ratio)
-        dataset = cut_vertically(dataset, w)
+        cube = cut_vertically(cube, w)
     else:
-        h = int(orig_w * (1 / aspect_ratio))
+        h = int(orig_w * aspect_ratio)
         w = orig_w
-        dataset = cut_horizontally(dataset, h)
+        cube = cut_horizontally(cube, h)
 
-    if radius is None:  # if no radius is given, use the minimum of h and w after cropping
-        radius = int(min([dataset.h, dataset.w]) / 2)
-
-    # # Plot before and after mask is applied
-    # plt.imshow(np.nanmean(dataset.X,
-    #                       0))  # The plot will appear in wrong orientation due to matplotlib expecting the indices in a certain order
-    # plt.show()
-
-    dataset.X = apply_circular_mask(dataset.X, dataset.w, dataset.h, radius=radius)
-    dataset.Y = apply_circular_mask(dataset.Y, dataset.w, dataset.h, radius=radius)
-    dataset.cube = apply_circular_mask(dataset.cube, dataset.w, dataset.h, radius=radius)
-
-    # # Sanity check plot
-    # plt.imshow(np.nanmean(dataset.X,
-    #                       0) + 1)  # matplotlib wants its dimensions in a different order, which makes the plot look like h and w are mixed
-    # plt.show()
-
-    return dataset
+    return cube
 
 
 def join_VIR_VIS_and_IR(vis_cube, ir_cube, vis_wavelengths, ir_wavelengths, vis_fwhms, ir_fwhms):
@@ -311,32 +296,84 @@ def solar_irradiance(distance: float, wavelengths=constants.ASPECT_wavelengths, 
     return final
 
 
-# TODO Make this function work, but later
-    # def ASPECTify(cube, wavelengths, VIS=False, NIR1=True, NIR2=True, SWIR=True):
-    #     """Take a spectral image and make it look like data from Milani's ASPECT"""
-    #     if VIS:
-    #         print('Sorry, the function can not currently work with the VIS portion of ASPECT')
-    #         print('Stopping execution')
-    #         exit(1)
-    #
-    #     # ASPECT wavelength vectors: change these values later, if the wavelengths change!
-    #     ASPECT_VIS_wavelengths = np.linspace(start=0.650, stop=0.950, num=14)
-    #     ASPECT_NIR1_wavelengths = np.linspace(start=0.850, stop=0.1250, num=14)
-    #     ASPECT_NIR2_wavelengths = np.linspace(start=1.200, stop=0.1600, num=14)
-    #     ASPECT_SWIR_wavelengths = np.linspace(start=1.650, stop=2.500, num=30)
-    #     # ASPECT FOVs in degrees
-    #     ASPECT_VIS_FOV = 10  # 10x10 deg square
-    #     ASPECT_NIR_FOV_w = 6.7  # width
-    #     ASPECT_NIR_FOV_h = 5.4  # height
-    #     ASPECT_SWIR_FOV = 5.85  # circular
-    #
-    #     if (NIR1 or NIR2) and SWIR:
-    #         # The largest is the SWIR circular FOV, and so it is the limiting factor
-    #         # Cut the largest possible rectangle where one side is circular FOV, other is width of NIR. Then divide
-    #         # along wavelength into NIR and SWIR, mask SWIR into circle and take mean spectrum, and cut NIR to size.
-    #         return None
-    #
-    #
-    # training_data_ASPECT = ASPECTify(training_data.cube, training_data.wavelengths)
+def ASPECT_NIR_SWIR_from_Dawn_VIR(cube, wavelengths, FWHMs, convert_rad2refl=True):
+    """Take a spectral image from Dawn VIR and make it look like data from Milani's ASPECT's NIR and SWIR"""
+
+    # Resample spectra to resemble ASPECT data
+    cube, wavelengths, FWHMs = ASPECT_resampling(cube, wavelengths, FWHMs)
+
+    # Convert radiances to I/F
+    if convert_rad2refl:
+        insolation = solar_irradiance(distance=constants.ceres_hc_dist, wavelengths=constants.ASPECT_wavelengths,
+                                            plot=False, resample=True)
+        cube = cube / insolation[:, 1]
+
+    # # Sanity check plot
+    # plt.imshow(cube[:, :, 20])
+    # plt.show()
+
+    # Crop the image to aspect ratio where one side is the larger of NIR FOV and one side is SWIR FOV
+    aspect_ratio = max(constants.ASPECT_NIR_FOV) / constants.ASPECT_SWIR_FOV
+    cube = crop2aspect_ratio(cube, aspect_ratio)
+
+    # Interpolate each channel to have more pixels: width from NIR width, height from what the height would be
+    height = constants.ASPECT_SWIR_equivalent_radius * 2  # height should be diameter of FOV, so 2 * radius
+    width = constants.ASPECT_NIR_channel_shape[1]
+    resized = np.zeros((height, width, cube.shape[2]))
+    for channel in range(cube.shape[2]):
+        resized[:, :, channel] = cv.resize(cube[:, :, channel], (width, height), interpolation=cv.INTER_AREA)
+    cube = resized
+
+    cube = apply_circular_mask(cube, height, width, masking_value=float('nan'))
+
+    test_cube = crop2aspect_ratio(cube, aspect_ratio=constants.ASPECT_NIR_channel_shape[0] /
+                                                            constants.ASPECT_NIR_channel_shape[1], keep_dim=1)
+
+    cube_short = test_cube[:, :, :int(len(wavelengths) / 2)]
+    cube_long = cube[:, :, int(len(wavelengths) / 2):]
+
+    SWIR_data = np.nanmean(cube_long, axis=(0, 1))
+    NIR_data = cube_short
+    test_data = test_cube
+
+    # # Sanity check plot
+    # plt.figure()
+    # plt.imshow(cube_short[:, :, 20])
+    # plt.figure()
+    # plt.imshow(cube_long[:, :, 20])
+    # plt.show()
+
+    return NIR_data, SWIR_data, test_data
+
+# TODO Make this work someday
+# def ASPECTify(cube, wavelengths, FWHMs, VIS=False, NIR1=True, NIR2=True, SWIR=True):
+#     """Take a spectral image and make it look like data from Milani's ASPECT"""
+#     if VIS:
+#         print('Sorry, the function can not currently work with the VIS portion of ASPECT')
+#         print('Stopping execution')
+#         exit(1)
+#
+#     # # ASPECT wavelength vectors: change these values later, if the wavelengths change!
+#     # ASPECT_VIS_wavelengths = np.linspace(start=0.650, stop=0.950, num=14)
+#     # ASPECT_NIR1_wavelengths = np.linspace(start=0.850, stop=0.1250, num=14)
+#     # ASPECT_NIR2_wavelengths = np.linspace(start=1.200, stop=0.1600, num=14)
+#     # ASPECT_SWIR_wavelengths = np.linspace(start=1.650, stop=2.500, num=30)
+#     # # ASPECT FOVs in degrees
+#     # ASPECT_VIS_FOV = 10  # 10x10 deg square
+#     # ASPECT_NIR_FOV_w = 6.7  # width
+#     # ASPECT_NIR_FOV_h = 5.4  # height
+#     # ASPECT_SWIR_FOV = 5.85  # circular
+#
+#     # Resample spectra to resemble ASPECT data
+#     cube, wavelengths, FWHMs = ASPECT_resampling(cube, wavelengths, FWHMs)
+#
+#     if (NIR1 or NIR2) and SWIR:
+#         # The largest is the SWIR circular FOV, and so it is the limiting factor
+#         # Cut the largest possible rectangle where one side is circular FOV, other is width of NIR. Then divide
+#         # along wavelength into NIR and SWIR, mask SWIR into circle and take mean spectrum, and cut NIR to size.
+#         return None
+
+
+
 
 
